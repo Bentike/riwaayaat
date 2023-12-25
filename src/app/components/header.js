fe8ca99a97b15d47788c09b63ae3dbfa4bbc1d0d
bentike <ajadiyusuf73@gmail.com>
2023-12-25 21:51:25 +0100

    initialized files

---
+++ src/app/components/header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <div>
+      <h1 className="text-center text-4xl py-5">Assalam Alaykum</h1>
+      <p className="text-center">
+        Welcome to Riwaayaat Online Learning Platform
+      </p>
+    </div>
+  );
+};
+
+export default Header;
