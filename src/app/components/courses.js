4f94cfd3278506f422ccdafbdddc23600e9840a6
bentike <ajadiyusuf73@gmail.com>
2023-12-26 10:22:11 +0100

    added courses component

---
+++ src/app/components/courses.js
@@ -0,0 +1,9 @@
+const Courses = () => {
+    return(
+        <div>
+            <h1 className="text-center text-3xl my-8">Courses</h1>
+        </div>
+    );
+}
+
+export default Courses;
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import Courses from "./components/courses";
 import Header from "./components/header";
 
 export default function Home () {
     return(
       <div>
         <Header/>
+        <Courses/>
       </div>
     );
 };
